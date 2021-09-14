1dd9463348d8b900b081c747996917c5fb2224db
Motyka <alex.motyka@cgi.com>
2021-09-14 09:41:06 -0400

    Add var for sshkey

--- variables.tf
+++ variables.tf
@@ -16,3 +16,8 @@ variable "location" {
     default = "centralus"
 }
 
+variable "sshkey" {
+    type = string
+    description = "public key to use for VM creation"
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDt2nZgXn+xL7gQiyfSMmTTtoALNTBTXw0Z34tMfYjv/FPbZzZStMNnCZ/BPCcOWVpVH61BGtX9dgHGLwbF4p01CegJlORlTule9wFcwxe3Sku/GyWJAfKLvotPiRNCQK+CD58bLjt/uLBjIEy0E5V8nDNS8+vzDD9HB2wxNgzlCLnGuFqhGZ08zgYP2HcmLxdWGv7+xnC8hS0olTMTKtwNVe1DpS8rP2BKIPjY084XwZ0PsLgwjvNFsSWwuy4MRP2cjSgd7GtfnDaaeQQKyI2FZz7GnOzPThfOy7lF8oxYzK/qfgfP7MGa8LALbwfdHjbTA7qH4UyPCj27Qwc4KgVGPK8egG7290mFAEhVpPNM7nZbXoR93cxvpD/wGIPyKNGv21gwBXa32E4Fz/1/WJMqn74k1ROorwXsdNNyAR7SCQTkMoe7ioOf9A+0t7q7Ti9OUbobxXRzJtb+zl3hunMOXne0mUl9ZLpRsscmXls87b8XXcUj1RxqkGdsG130BO8= alex.motyka@CA-L4F6H6D3"
+}
