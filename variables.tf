0901cc7868f8a355aa7db9c78e94706652c8d57f
Motyka <alex.motyka@cgi.com>
2021-09-14 09:38:22 -0400

    Set default values

--- variables.tf
+++ variables.tf
@@ -1,15 +1,18 @@
 variable "subscriptionID" {
     type = string
     description = "Variable for our resource group"
+    default = "7646415c-fb28-4c56-aee7-e29843c48c3d"
 }
 
 variable "resourceGroupName" {
     type = string
     description = "name of resource group"
+    default = "cloud_bootcamp_alex_rg"
 }
 
 variable "location" {
     type = string
     description = "location of your resource group"
+    default = "centralus"
 }
 
